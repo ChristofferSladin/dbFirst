3ce1f9737963b9b7ad23310cb0f687d62f5a79e7
Christoffer Sladin <102432146+ChristofferSladin@users.noreply.github.com>
2022-12-07 21:24:43 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using ConsoleApp1.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+        var config = builder.Build();
+
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        var options = new DbContextOptionsBuilder<RichardsAuctionSiteContext>();
+        options.UseSqlServer(connectionString);
+
+        using (var dbContext = new RichardsAuctionSiteContext(options.Options))
+        {
+            foreach (var user in dbContext.Users.OrderBy(u => u.City))
+            {
+                Console.WriteLine($"{user.Name}: {user.City}");
+            }
+        }
+    }
+}
